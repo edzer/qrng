4d5fb213e5793c5b2aa4c21d46a74fa14ae586bf
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2013-11-05 22:02:03 +0100

    tidy

--- src/qrng.c
+++ src/qrng.c
@@ -1,6 +1,6 @@
-# include <R.h>
-# include <Rdefines.h>
-# include <Rinternals.h>
+#include <R.h>
+#include <Rdefines.h>
+#include <Rinternals.h>
 #include <R_ext/Rdynload.h>
 
 # include <libQRNG.h>
