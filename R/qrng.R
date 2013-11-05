3554db89afe7798bfdd9f6d0f085dd089136ebd6
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2013-11-05 18:04:29 +0100

    allow setting qrng as the user-define RNG for R
    added function setup_QRNG("user", "pw") that installs
    the QRNG as default RNG in R, i.e. subsequent calls to runif
    or anything else will use this RNG, instead of R's own. See
    also ?Random.user for details.

--- R/qrng.R
+++ R/qrng.R
@@ -1,4 +1,13 @@
-quantum_runif = function(n, min=0, max=1,..., user, password) {
+quantum_runif = function(n, min = 0.0, max = 1.0,..., user, password) {
 	ret = .Call("QRNG", as.integer(n), user, password)
-	ret * max + min
+	min + ret * max
+}
+
+setup_QRNG = function(usr, passwd, ssl = 0L, bufsize = 1e6) {
+	stopifnot(!missing(usr))
+	stopifnot(!missing(passwd))
+	stopifnot(ssl == 0 || ssl == 1)
+	stopifnot(bufsize > 0)
+	.Call("setup_QRNG", usr, passwd, as.integer(ssl), as.integer(bufsize))
+	RNGkind("user")
 }
--- src/qrng.c
+++ src/qrng.c
@@ -21,6 +21,10 @@ SEXP QRNG(SEXP s_n, SEXP s_usr, SEXP s_passwd) {
 	*/
 	d = (double *) R_alloc((size_t) n, sizeof(double));
 	err = qrng_connect_and_get_double_array(usr, passwd, d, n, &n_ret);
+	if (err)
+		PROBLEM "qrng error: %s", qrng_error_strings[err] ERROR;
+	if (n_ret == 0)
+		PROBLEM "zero random numbers retrieved: wrong login or passwd?" ERROR;
 	if (n_ret != n)
 		PROBLEM "random numbers retrieved: %d", n_ret ERROR;
 	for (i = 0; i < n; i++)
@@ -29,11 +33,48 @@ SEXP QRNG(SEXP s_n, SEXP s_usr, SEXP s_passwd) {
 	return(rn);
 }
 
+static int connected = 0, buf_size = 0, i = -1;
+static double *buf = NULL;
+
+SEXP setup_QRNG(SEXP s_usr, SEXP s_passwd, SEXP s_ssl, SEXP s_bufsize) {
+	int err, ssl;
+	ssl = INTEGER_POINTER(s_ssl)[0];
+	if (ssl)
+		err = qrng_connect_SSL(CHAR(STRING_ELT(s_usr, 0)), CHAR(STRING_ELT(s_passwd, 0)));
+	else
+		err = qrng_connect(CHAR(STRING_ELT(s_usr, 0)), CHAR(STRING_ELT(s_passwd, 0)));
+	if (err) {
+		PROBLEM "qrng error: %s", qrng_error_strings[err] ERROR;
+		connected = 0;
+	} else
+		connected = 1;
+	buf_size = INTEGER_POINTER(s_bufsize)[0];
+	buf = (double *) realloc(buf, buf_size * sizeof(double));
+	if (buf == NULL)
+		PROBLEM "cannot allocate buffer for qrng" ERROR;
+	return(s_ssl);
+}
+
+double *user_unif_rand() {
+	int err, rcvd;
+	if (i == buf_size || i < 0) {
+		err = qrng_get_double_array(buf, buf_size, &rcvd);
+		if (err)
+			PROBLEM "qrng error: %s", qrng_error_strings[err] ERROR;
+		if (rcvd != buf_size)
+			PROBLEM "recieved qrng numbers: %d", rcvd ERROR;
+		i = 0;
+	}
+	return(&(buf[i++]));
+}
+
 static R_CallMethodDef CallEntries[] = {
     {"QRNG", (DL_FUNC) &QRNG, 3},
+	{"setup_QRNG", (DL_FUNC) setup_QRNG, 4},
     {NULL, NULL, 0}
 };
 static const R_CMethodDef CEntries[]  = {
+	{"user_unif_rand", (DL_FUNC) &user_unif_rand, 0},
     {NULL, NULL, 0}
 };
 
